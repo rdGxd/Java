54f69764f551e060c63079a78aa68dba1f1cf8f6
Rodrigo Silva <rdg.hora1@gmail.com>
2024-02-24 09:46:15 -0300

    Exemplo completo

--- Exemplo2/src/application/App.java
+++ Exemplo2/src/application/App.java
@@ -19,7 +19,25 @@ public class App {
     System.out.print("Quantity: ");
     product.quantity = sc.nextInt();
 
-    System.out.println(product);
+    System.out.println();
+    System.out.println("Product data: " + product);
+
+    System.out.println();
+    System.out.println("Enter the number of products to be added in stock: ");
+    int quantity = sc.nextInt();
+    product.addProducts(quantity);
+
+    System.out.println();
+    System.out.println("Updated data: " + product);
+
+    System.out.println();
+    System.out.println("Enter the number of products to be removed from stock: ");
+    quantity = sc.nextInt();
+    product.removeProducts(quantity);
+
+    System.out.println();
+    System.out.println("Updated data: " + product);
+
     sc.close();
   }
 
