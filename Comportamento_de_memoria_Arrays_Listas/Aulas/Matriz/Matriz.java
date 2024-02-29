50b61bbb73fd10b558d8360609be9145b6b0bc38
Rodrigo Silva <rdg.hora1@gmail.com>
2024-02-29 10:17:25 -0300

    Finalizado

--- Comportamento_de_memoria_Arrays_Listas/Aulas/Matriz/Matriz.java
+++ Comportamento_de_memoria_Arrays_Listas/Aulas/Matriz/Matriz.java
@@ -1,7 +1,4 @@
-import java.util.Locale;
-import java.util.Scanner;
-
-public class App {
+public class Matriz {
   public static void main(String[] args) throws Exception {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
---
+++ Comportamento_de_memoria_Arrays_Listas/Exercicios/Exercicio14/src/App.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite um número de linhas");
+    int m = sc.nextInt();
+    int n = sc.nextInt();
+    int[][] mat = new int[m][n];
+
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[i].length; j++) {
+        mat[i][j] = sc.nextInt();
+      }
+    }
+
+    System.err.println();
+    System.out.print("Qual número deseja as informações ? ");
+    int x = sc.nextInt();
+
+    System.err.println();
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[i].length; j++) {
+        if (mat[i][j] == x) {
+          System.out.printf("Position: %d,%d%n", i, j);
+          System.out.println("Position " + i + "," + j + ":");
+          if (j > 0) {
+            System.out.println("Left: " + mat[i][j - 1]);
+          }
+          if (i > 0) {
+            System.out.println("Up: " + mat[i - 1][j]);
+          }
+          if (j < mat[i].length - 1) {
+            System.out.println("Right: " + mat[i][j + 1]);
+          }
+          if (i < mat.length - 1) {
+            System.out.println("Down: " + mat[i + 1][j]);
+          }
+        }
+      }
+    }
+
+    sc.close();
+  }
+}
