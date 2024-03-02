9f46be380734cdfd3b1f299b14e589155b21e94b
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-02 10:23:14 -0300

    DataHora

--- Topicos_especiais_Java_Data-Hora/src/App.java
+++ Topicos_especiais_Java_Data-Hora/src/App.java
@@ -1,8 +1,10 @@
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class App {
   public static void main(String[] args) throws Exception {
@@ -33,6 +35,23 @@ public class App {
     LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
     LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
 
+    // Calculo com data-hora
+    LocalDate pastWeekLocalDate = d04.minusDays(7);
+    LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+    LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+    LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+    Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+    Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+    System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+    System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+    System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+    System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+    System.out.println("pastWeekInstant = " + pastWeekInstant);
+    System.out.println("nextWeekInstant = " + nextWeekInstant);
+
     System.out.println("D01 = " + d01);
     System.out.println("D02 = " + d02);
     System.out.println("D03 = " + d03);
@@ -49,16 +68,27 @@ public class App {
     System.out.println("D14 = " + fmt4.format(d05));
     System.out.println("D14 = " + fmt5.format(d06));
     System.out.println("D15 = " + fmt5.format(d06));
+
     System.out.println("R1 = " + r1);
     System.out.println("R2 = " + r2);
     System.out.println("R3 = " + r3);
     System.out.println("R4 = " + r4);
+
     System.out.println("D04 DIA = " + d04.getDayOfMonth());
     System.out.println("D04 MES = " + d04.getMonthValue());
     System.out.println("D04 ANO = " + d04.getYear());
-
     System.out.println("D05 HORA = " + d05.getHour());
     System.out.println("D05 MINUTE = " + d05.getMinute());
 
+    // Calculando duração entre duas data-hora
+    Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+    Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+    Duration t3 = Duration.between(pastWeekInstant, d06);
+    Duration t4 = Duration.between(d06, pastWeekInstant);
+
+    System.out.println("DURATION T1 = " + t1.toDays());
+    System.out.println("DURATION T2 = " + t2.toDays());
+    System.out.println("DURATION T3 = " + t3.toDays());
+    System.out.println("DURATION T4 = " + t4.toDays());
   }
 }
