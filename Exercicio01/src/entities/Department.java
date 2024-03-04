5edb08973cd5cfacb5bb1ae68ca249b91d5acaa6
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-04 10:59:08 -0300

    Part1

---
+++ Exercicio01/src/entities/Department.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Department {
+  private String name;
+
+  public Department() {
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+}
