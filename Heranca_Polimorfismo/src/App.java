ae073e744a026ecd3839e416f32bbda8f3d809d0
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-05 12:32:36 -0300

    Herança

--- Heranca_Polimorfismo/src/App.java
+++ Heranca_Polimorfismo/src/App.java
@@ -1,5 +1,8 @@
+import entities.BusinessAccount;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    BusinessAccount account = new BusinessAccount();
+
+  }
 }
