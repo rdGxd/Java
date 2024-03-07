4fc9e52d79119efb9dab8a73ed0832b458be8031
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-07 11:00:34 -0300

    teste

--- Heranca_Polimorfismo/src/Heranca_Upcasting_Downcasting.java
+++ Heranca_Polimorfismo/src/Heranca_Upcasting_Downcasting.java
@@ -8,7 +8,7 @@ public class Heranca_Upcasting_Downcasting {
     BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
     // UPCASTING
-    Account acc1 = bacc;
+    Account acc = bacc;
     Account acc2 = new BusinessAccount(1003, "Alex", 0.0, 200.0);
     Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
 
