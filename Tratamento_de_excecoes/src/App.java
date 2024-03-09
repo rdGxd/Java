97d9f8ca0889e658b8d92b6d1b5cc220a9eb33a0
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-09 08:29:39 -0300

    try_catch

--- Tratamento_de_excecoes/src/App.java
+++ Tratamento_de_excecoes/src/App.java
@@ -1,5 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+
+    try {
+      String[] vect = sc.nextLine().split(" ");
+      int position = sc.nextInt();
+      System.out.println(vect[position]);
+
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Invalid position!");
+    } catch (InputMismatchException e) {
+      System.out.println("Input error!");
     }
+
+    System.out.println("End of program");
+
+    sc.close();
+  }
 }
