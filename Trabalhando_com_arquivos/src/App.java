2ebbfac2801309231ed8ad33964be09cd4b6926a
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-18 10:38:28 -0300

    Basic file reading

--- Trabalhando_com_arquivos/src/App.java
+++ Trabalhando_com_arquivos/src/App.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    File file = new File("C:\\Users\\rodri\\Documents\\in.txt");
+    Scanner sc = null;
+    try {
+      sc = new Scanner(file);
+      while (sc.hasNextLine()) {
+        System.out.println(sc.nextLine());
+      }
+    } catch (Exception e) {
+      System.out.println("Error" + e.getMessage());
+    } finally {
+      if (sc != null) {
+        sc.close();
+      }
     }
+  }
 }
