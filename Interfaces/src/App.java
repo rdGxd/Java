3dc2a02fea6d2c762a6a84161bd4fc0510147c67
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-19 11:25:25 -0300

    Problema resolvido sem usar interface

--- Interfaces/src/App.java
+++ Interfaces/src/App.java
@@ -1,5 +1,44 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.CarRental;
+import entities.Vehicle;
+import services.BrazilTaxService;
+import services.RentalService;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+    System.out.println("Entre com os dados do aluguel");
+    System.out.print("Modelo do carro: ");
+    String carModel = sc.nextLine();
+    System.err.print("Retirada (dd/MM/yyyy hh:mm): ");
+    LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+    System.err.print("Retorno (dd/MM/yyyy hh:mm): ");
+    LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+    CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+    System.out.print("Entre com o preço por hora: ");
+    double pricePerHour = sc.nextDouble();
+    System.out.print("Entre com o preço por dia: ");
+    double pricePerDay = sc.nextDouble();
+
+    RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+    rentalService.processInvoice(cr);
+
+    System.out.println("FATURA:");
+    System.out.println("Pagamento básico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+    System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+    System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+    sc.close();
+  }
 }
---
+++ Interfaces/src/services/BrazilTaxService.java
@@ -0,0 +1,12 @@
+package services;
+
+public class BrazilTaxService {
+
+  public double tax(double amount) {
+    if (amount <= 100.00) {
+      return amount * 0.20;
+    } else {
+      return amount * 0.15;
+    }
+  }
+}
