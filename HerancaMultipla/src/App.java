6908eeee87af9574ba1603d5e4d8dfc26db5a31f
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-21 13:11:44 -0300

    Herança múltipla e o problema do diamante

---
+++ HerancaMultipla/src/App.java
@@ -0,0 +1,23 @@
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    System.out.println();
+    ConcretePrinter p = new ConcretePrinter("1080");
+    p.processDoc("My Letter");
+    p.print("My Letter");
+
+    System.out.println();
+    ConcreteScanner s = new ConcreteScanner("2003");
+    s.processDoc("My Email");
+    System.out.println("Scan result:" + s.scan());
+
+    System.out.println();
+    ComboDevice c = new ComboDevice("2081");
+    c.processDoc("My dissertation");
+    c.print("My dissertation");
+    System.out.println("Combo Scan result: " + c.scan());
+  }
+}
