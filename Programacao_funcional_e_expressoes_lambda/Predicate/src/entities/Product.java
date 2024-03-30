87e1cbf7d7390bda194e4f2a615b3ef736006bb9
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-30 08:45:34 -0300

    Interface funcional -> Predicate

--- Programacao_funcional_e_expressoes_lambda/Predicate/src/entities/Product.java
+++ Programacao_funcional_e_expressoes_lambda/Predicate/src/entities/Product.java
@@ -25,6 +25,17 @@ public class Product {
         this.price = price;
     }
 
+
+    // Método estátacio ele trabalha com o produto que é passado como argumento para ele
+    public static  boolean staticProductPredicate(Product p) {
+        return p.getPrice() >= 100.00;
+    }
+
+    // Método nao estático é o método da instancia vai trabalhar com o proprio objeto
+    public boolean nonStaticProductPredicate() {
+        return getPrice() >= 100.00;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
