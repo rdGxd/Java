5b758e4b3be4795b8dbddebfac802f86beda9c72
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-05 10:31:58 -0300

    Deletar dados

---
+++ Acesso_A_Banco_de_dados_com_JDBC/jdbc5_DeletarDados/src/Main.java
@@ -0,0 +1,26 @@
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement("DELETE FROM department WHERE id = ?");
+            st.setInt(1, 2);
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
